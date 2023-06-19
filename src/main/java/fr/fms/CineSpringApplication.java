739ed06eadb7f4c1fca27c0f939a95b77e4d3866
Francois Supervielle-Brouques <francoisup40@gmail.com>
2023-06-19 10:08:59 +0200

    first

---
+++ src/main/java/fr/fms/CineSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSpringApplication.class, args);
+	}
+
+}
